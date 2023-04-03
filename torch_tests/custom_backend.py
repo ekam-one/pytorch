7ec2ee59bad9dd4f58dd7b89cbea72f22584ebf8
“agentDoubleOSeven” <ekamonetwo@gmail.com>
2023-04-03 16:39:06 +0530

    POC of custom backend Autograd for a simple kernel

--- torch_tests/custom_backend.py
+++ torch_tests/custom_backend.py
@@ -1,15 +1,29 @@
-from typing import List
+import warnings
 import torch
 import torch._dynamo
+from torch._dynamo.backends.common import aot_autograd
+from functorch.compile import make_boxed_func
 import enum
 from typing import Callable
 import subprocess
 from ctypes import cdll
 from ctypes import c_void_p
 from torch import empty_strided
-from torch._dynamo.backends.common import aot_autograd
-from functorch.compile import make_boxed_func
+from typing import List
+from torch._dispatch.python import enable_python_dispatcher
+from functorch import make_fx
+from torch.fx import immutable_collections, Interpreter
+from torch.nn.utils import stateless
+import torch.fx.traceback as fx_traceback
+import torch.utils._pytree as pytree
+from torch._functorch.aot_autograd import create_forward_or_joint_functionalized
+from torch._functorch.aot_autograd import CompiledRuntimeMetadata
+from torch._functorch.aot_autograd import run_functionalized_fw_and_collect_metadata
+from torch._functorch.aot_autograd import merge_view_inputs
+from torch import Tensor
 from torch._inductor.decomposition import select_decomp_table
+import torch.fx._symbolic_trace as fx
+from torch._functorch.aot_autograd import AOTConfig
 
 class MlirDType(enum.Enum):
     f32 = 1
@@ -108,40 +122,51 @@ class Codegen:
             self.prefix = self.prefix[:-1]
         self.code.append(self.prefix + "}")
 
+    def primals_1(self, node):
+        pass
+
     def placeholder(self, node):
         pass
 
     def output(self, node):
-        assert len(node.all_input_nodes) == 1, "Expecting only one input for sin node"
-        in_node = node.all_input_nodes[0]
-        memref_type = self.node_types[in_node].get_memref_string()
-        tempnode_name = node.name + "_tmp"
-        string = (
-            self.prefix
-            + "%"
-            + tempnode_name
-            + " = bufferization.to_memref %"
-            + node.name
-            + " : "
-            + memref_type
-            + "\n"
-        )
+        for i in range(len(node.all_input_nodes)):
+            in_node = node.all_input_nodes[i]
+            memref_type = self.node_types[in_node].get_memref_string()
+            tempnode_name = in_node.name + "_tmp"
+            string = (
+                self.prefix
+                + "%"
+                + tempnode_name
+                + " = bufferization.to_memref %"
+                + ("out_"+in_node.name)
+                + " : "
+                + memref_type
+                + "\n"
+            )
 
-        self.code.append(string)
-        string = (
-            self.prefix
-            + "memref.tensor_store %"
-            + in_node.name
-            + ", %"
-            + tempnode_name
-            + " : "
-            + memref_type
-            + "\n"
-        )
-        self.code.append(string)
+            self.code.append(string)
+            string = (
+                self.prefix
+                + "memref.tensor_store %"
+                + in_node.name
+                + ", %"
+                + tempnode_name
+                + " : "
+                + memref_type
+                + "\n"
+            )
+            self.code.append(string)
 
     def sin(self, node):
         string = self.prefix + "%" + node.name + " = math.sin %"
+        assert len(node.all_input_nodes) == 1, "Expecting only one input for sin node"
+        in_node = node.all_input_nodes[0]
+        assert in_node in self.node_types
+        string += in_node.name + " : " + str(self.node_types[in_node]) + "\n"
+        self.code.append(string)
+
+    def cos(self, node):
+        string = self.prefix + "%" + node.name + " = math.cos %"
         assert len(node.all_input_nodes) == 1, "Expecting only one input for sin node"
         in_node = node.all_input_nodes[0]
         assert in_node in self.node_types
@@ -169,6 +194,49 @@ class Codegen:
         )
         self.code.append(string)
 
+    def mul(self, node):
+        string = self.prefix + "%" + node.name + " = arith.mulf "
+        assert len(node.all_input_nodes) == 2, "Expecting only one input for mul node"
+        in_node1 = node.all_input_nodes[0]
+        in_node2 = node.all_input_nodes[1]
+        assert in_node1 in self.node_types and in_node2 in self.node_types
+        string += (
+            "%"
+            + in_node1.name
+            + ", %"
+            + in_node2.name
+            + " : "
+            + str(self.node_types[in_node1])
+            # + ", "
+            # + str(self.node_types[in_node2])
+            # + ") -> "
+            # + str(self.node_types[in_node1])
+            + "\n"
+        )
+        self.code.append(string)
+
+
+    def sub(self, node):
+        string = self.prefix + "%" + node.name + " = arith.subf "
+        assert len(node.all_input_nodes) == 2, "Expecting only one input for sub node"
+        in_node1 = node.all_input_nodes[0]
+        in_node2 = node.all_input_nodes[1]
+        assert in_node1 in self.node_types and in_node2 in self.node_types
+        string += (
+            "%"
+            + in_node1.name
+            + ", %"
+            + in_node2.name
+            + " : "
+            + str(self.node_types[in_node1])
+            # + ", "
+            # + str(self.node_types[in_node2])
+            # + ") -> "
+            # + str(self.node_types[in_node1])
+            + "\n"
+        )
+        self.code.append(string)
+
     def call_function(self, node):
         target_fn = str()
         if isinstance(node.target, Callable):
@@ -177,10 +245,17 @@ class Codegen:
             target_fn = node.target
         else:
             assert False, "Unknown target type in call_function"
+        
+        if target_fn.endswith(".default"):
+            target_fn = target_fn[:-len(".default")]
+
+        if target_fn.endswith(".Tensor"):
+            target_fn = target_fn[:-len(".Tensor")]
 
         getattr(self, target_fn)(node)
 
     def codegen_node(self, node):
+        print(node)
         getattr(self, node.op)(node)
 
     def unknwon(self, node):
@@ -202,6 +277,7 @@ class MlirFxBackend:
         self.inputs = example_inputs
         self.arg_types = []
         self.arg_names = []
+        self.output_types = []
         self.kernel_file_name = "torch_kernel.mlir"
         self.kernel_name = "kernel"
         self.lib_name = "./torch_library.so"
@@ -238,18 +314,24 @@ class MlirFxBackend:
 
     def __call__(self, *args, **kwargs):
 
-        out_arg = empty_strided(
-            self.output_type.shape, (1,), dtype=torch_type_dict[self.output_type.dtype]
-        )
+        print("__call__ custom called")
+
+        output_args = []
+
+        for out_type in self.output_types:
+            out_arg =  empty_strided(out_type.shape, (1,), dtype=torch_type_dict[out_type.dtype])
+            output_args.append(out_arg)
 
         actual_args = list(args)
         ptrs = []
         for a_arg in actual_args:
             ptrs.append(c_void_p(a_arg.data_ptr()))
+        
+        for out_arg in output_args:
+           ptrs.append(c_void_p(out_arg.data_ptr())) 
 
-        ptrs.append(c_void_p(out_arg.data_ptr()))
         self.kernel(*ptrs)
-        return (out_arg,)
+        return tuple(output_args)
 
     def infer_arg_type(self, node, i):
         input = self.inputs[i]
@@ -288,15 +370,12 @@ class MlirFxBackend:
 
     def infer_output(self, node):
         input_nodes = node.all_input_nodes
-        assert (
-            len(input_nodes) == 1
-        ), "Output node with only one argument is handled for now"
-
-        assert input_nodes[0] in self.node_types
-        self.node_types[node] = self.node_types[input_nodes[0]]
-        self.output_type = self.node_types[node]
-        self.arg_types.append(self.node_types[node])
-        self.arg_names.append(node.name)
+        
+        for i in range(len(input_nodes)):
+           assert input_nodes[i] in self.node_types
+           self.output_types.append(self.node_types[input_nodes[i]])
+           self.arg_types.append(self.node_types[input_nodes[i]])
+           self.arg_names.append("out_"+input_nodes[i].name)
 
     def compile_and_load(self):
         i = 0
@@ -314,6 +393,8 @@ class MlirFxBackend:
             self.arg_names,
             self.node_types,
         )
+
+        print(self.gm.graph)
         for node in self.gm.graph.nodes:
             cg.codegen_node(node)
         cg.codegen_func_epilogue()
@@ -322,37 +403,11 @@ class MlirFxBackend:
         return self.lower_and_load()
 
 
-# def fn(x, y):
-#     a = torch.sin(x)
-#     b = torch.sin(y)
-#     return a + b
-
-
-# compiled = torch.compile(fn, backend=MlirFxBackend)
-
-# print("Done with compilation, now jit")
-# a = torch.randn(100)
-# b = torch.randn(100)
-# print("Output from custom : ", compiled(a, b))
-# print("Output from local : ", fn(a, b))
-
-
-import torch
-
-
-# def fn(x, y):
-#     a = torch.sin(x)
-#     b = torch.sin(y)
-#     return a + b
-
-# new_fn = torch.compile(fn, backend=MlirFxBackend)
-# input_tensor = torch.randn(100, requires_grad=True)
-# out = new_fn(input_tensor, input_tensor)
-# print(out.sum().backward())
 
 def my_compiler(gm, example_inputs):
   backend = MlirFxBackend(gm, example_inputs)
-  return make_boxed_func(backend.kernel)
+  return make_boxed_func(backend.__call__)
+
 
 my_backend = aot_autograd(
             fw_compiler=my_compiler,
@@ -362,10 +417,14 @@ my_backend = aot_autograd(
 
 
 def fn(x):
-    return torch.softmax(x, -1)
+    return torch.sin(x)
+    #return torch.softmax(x, -1)
+
+input_tensor = torch.range(0, 99, requires_grad=True)
 
 model_opt = torch.compile(fn, backend=my_backend)
 
-input_tensor = torch.randn(100, requires_grad=True)
 out = model_opt(input_tensor)
+print(out)
+
 print(out.sum().backward())
